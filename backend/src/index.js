a0e118d68b18d3efdc895cde98a2383e4f7b9767
Victor Mazzola <victormazzola2019@gmail.com>
2025-02-08 17:15:15 -0300

    chore: setup MongoDB connection and environment variables

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,17 @@
 import express from "express";
+import dotenv from "dotenv";
+
+import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 
+dotenv.config();
 const app = express();
+
+const PORT = process.env.PORT
 
 app.use("/api/auth", authRoutes)
 
-app.listen(5001, () => {
-  console.log("Server is running on port 5001");
+app.listen(PORT, () => {
+  console.log("Server is running on port PORT:" + PORT);
+  connectDB()
 });

778fb00c7f783b02dea9e4cf46fa80836782e24b
Victor Mazzola <victormazzola2019@gmail.com>
2025-02-08 16:39:57 -0300

    chore: move auth route to controllers and update import

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,17 +1,11 @@
 import express from "express";
+import { login, logout, signup } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-router.post("/signup", (req, res) => {
-  res.send("signup route");
-});
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout",logout );
 
-router.post("/login", (req, res) => {
-  res.send("login route");
-});
-
-router.post("/logout", (req, res) => {
-  res.send("logout route");
-});
 
 export default router;

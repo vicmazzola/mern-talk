288c88745984a662607d4eb8083cde0de2159650
Victor Mazzola <victormazzola2019@gmail.com>
2025-02-09 16:19:44 -0300

    feat: implement signup, login, and profile pages; setup toast notifications and navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,7 @@ import { useAuthStore } from "./store/useAuthStore";
 import { useEffect } from "react";
 
 import { Loader } from "lucide-react";
+import { Toaster } from "react-hot-toast";
 
 
 const App = () => {
@@ -42,6 +43,11 @@ const App = () => {
         <Route path="/settings" element={<SettingsPage />} />
         <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
       </Routes>
+
+
+      <Toaster />
+
+
     </div>
   );
 };

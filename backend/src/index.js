35bd150bae35b0aee7fa53a668fe76a8df6a5af8
Victor Mazzola <victormazzola2019@gmail.com>
2025-02-09 11:15:47 -0300

    feat: add message model, routes, and controller

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
 dotenv.config();
 const app = express();
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes)
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on port PORT:" + PORT);

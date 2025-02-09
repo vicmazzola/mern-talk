c1fbf31093bd70c10ae2ff1da88848b4845bddc2
Victor Mazzola <victormazzola2019@gmail.com>
2025-02-09 16:52:21 -0300

    feat: add DaisyUI themes, constants, and theme management with zustand; create settings page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import ProfilePage from "./pages/ProfilePage";
 
 import { Routes, Route, Navigate } from "react-router-dom";
 import { useAuthStore } from "./store/useAuthStore";
+import { useThemeStore } from "./store/useThemeStore";
 import { useEffect } from "react";
 
 import { Loader } from "lucide-react";
@@ -15,11 +16,8 @@ import { Toaster } from "react-hot-toast";
 
 
 const App = () => {
-
-
   const {authUser, checkAuth,isCheckingAuth} =  useAuthStore();
-
-  
+  const { theme } = useThemeStore();  
   useEffect(() => {
     checkAuth();
   }, [checkAuth]);
@@ -34,7 +32,7 @@ const App = () => {
 
 
   return (
-    <div>
+    <div data-theme={theme}>
       <Navbar />
       <Routes>
         <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />

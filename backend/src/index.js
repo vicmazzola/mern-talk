e0a36ceff77fdf4d13b0323e9bb3aaa27b57237f
Victor Mazzola <victormazzola2019@gmail.com>
2025-02-10 08:30:45 -0300

    update title and svg

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import cors from "cors"
+import cors from "cors";
+
+import path from "path";
+
 import { connectDB } from "./lib/db.js";
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-import {app, server} from "./lib/socket.js" 
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -21,11 +25,18 @@ app.use(
   })
 );
 
-
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
 server.listen(PORT, () => {
-  console.log("Server is running on port PORT:" + PORT);
-  connectDB()
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
 });
